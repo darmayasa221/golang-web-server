4b24b90543a68a1e833fcb59b80c33a351171724
darmayasa221 <darmayasa221@gmail.com>
2023-05-02 10:24:22 +0800

    implemented PlayerServer function

--- server.go
+++ server.go
@@ -1,3 +1,19 @@
 package main
 
-func PlayerServer() {}
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	if player == "Paper" {
+		fmt.Fprintf(w, "20")
+		return
+	}
+	if player == "Floyd" {
+		fmt.Fprintf(w, "10")
+		return
+	}
+}
